6c8336916193206a1d7484b5439ce7f8503e31ec
laas2004 <113628032+laas2004@users.noreply.github.com>
2026-04-03 14:20:18 +0530

    Update supabase-client.ts

--- app/src/lib/supabase-client.ts
+++ app/src/lib/supabase-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // HARDCODE your values here temporarily
-const supabaseUrl = "https://tdaueyovkerhmujkwgjl.supabase.co";
-const supabaseKey = "sb_secret_x8ZqXYu7Yiwo22kWPKuRiA_oRTAn1Pg";
+const supabaseUrl = "keep your own ";
+const supabaseKey = "keep your own";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
